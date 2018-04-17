50a2c5c2ef0dc2d018e82fb32e9f8802a6c85738
aluno <aluno@CEMCPESL04007.estacio.acad>
2018-04-16 21:21:30 -0300

    Adicionando Menus

---
+++ DicionarioDaviNilson/src/br/estacio/prii/dicionario/frame/MenuItemListener.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.estacio.prii.dicionario.frame;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+/**
+ *
+ * @author aluno
+ */
+class MenuItemListener implements ActionListener {
+    public void actionPerformed(ActionEvent e) {            
+         System.out.print(e.getActionCommand() + " MenuItem clicked.");
+      }    
+}
