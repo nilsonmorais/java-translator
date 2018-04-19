2ab676f6dd932ef4aececa8aa0dd5ccfd14112e5
aluno <aluno@CEMCPESL04006.estacio.acad>
2018-04-18 21:14:33 -0300

    Radio Button Group

--- DicionarioDaviNilson/src/br/estacio/prii/dicionario/frame/FrameCadastrar.java
+++ DicionarioDaviNilson/src/br/estacio/prii/dicionario/frame/FrameCadastrar.java
@@ -8,6 +8,7 @@ package br.estacio.prii.dicionario.frame;
 import br.estacio.prii.dicionario.utils.Utils;
 import static br.estacio.prii.dicionario.utils.Utils.getIconForButton;
 import java.awt.GridBagLayout;
+import javax.swing.ButtonGroup;
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
@@ -22,17 +23,26 @@ class FrameCadastrar extends JPanel {
 
     public FrameCadastrar() {
         super();
+        
         this.setBorder(javax.swing.BorderFactory.createTitledBorder("Cadastrar"));
         this.setLayout(new GridBagLayout());
         
         JTextField textPalavra = new JTextField("",15);
         JTextField textTraducao = new JTextField("",15);
         
+        ButtonGroup radioGroup = new ButtonGroup();
+        JRadioButton radioIngles = new JRadioButton("Inglês");
+        radioIngles.setSelected(true);
+        JRadioButton radioPortugues = new JRadioButton("Português");
+        radioGroup.add(radioIngles);
+        radioGroup.add(radioPortugues);
+        
         this.add(new JLabel("Palavra:"), Utils.createGridBagConstraints(0, 0, Utils.pad10, null));
         this.add(textPalavra, Utils.createGridBagConstraints(1, 0));
         
-        this.add(new JRadioButton("Inglês"), Utils.createGridBagConstraints(2, 0, Utils.pad10, null));
-        this.add(new JRadioButton("Português"), Utils.createGridBagConstraints(3, 0));
+        this.add(radioIngles, Utils.createGridBagConstraints(2, 0, Utils.pad10, null));
+        this.add(radioPortugues, Utils.createGridBagConstraints(3, 0, Utils.pad10, null));
+        
         
         this.add(new JLabel("Tradução:"), Utils.createGridBagConstraints(0, 1, Utils.pad10, null));
         this.add(textTraducao, Utils.createGridBagConstraints(1, 1));
--- DicionarioDaviNilson/src/br/estacio/prii/dicionario/frame/FrameTraduzir.java
+++ DicionarioDaviNilson/src/br/estacio/prii/dicionario/frame/FrameTraduzir.java
@@ -8,6 +8,7 @@ package br.estacio.prii.dicionario.frame;
 import br.estacio.prii.dicionario.utils.Utils;
 import static br.estacio.prii.dicionario.utils.Utils.getIconForButton;
 import java.awt.GridBagLayout;
+import javax.swing.ButtonGroup;
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
@@ -28,13 +29,20 @@ class FrameTraduzir extends JPanel {
         JTextField textPalavra = new JTextField("",15);
         JTextField textTraducao = new JTextField("",15);
         
+        ButtonGroup radioGroup = new ButtonGroup();
+        JRadioButton radioIngles = new JRadioButton("Inglês");
+        radioIngles.setSelected(true);
+        JRadioButton radioPortugues = new JRadioButton("Português");
+        radioGroup.add(radioIngles);
+        radioGroup.add(radioPortugues);
+        
         this.add(new JLabel("Palavra:"), Utils.createGridBagConstraints(0, 0, Utils.pad10, null));
         this.add(textPalavra, Utils.createGridBagConstraints(1, 0));
         
 
         this.add(new JLabel("Traduzir para:"), Utils.createGridBagConstraints(0, 1, Utils.pad10, null));
-        this.add(new JRadioButton("Inglês"), Utils.createGridBagConstraints(1, 1, Utils.pad10, null));
-        this.add(new JRadioButton("Português"), Utils.createGridBagConstraints(2, 1));
+        this.add(radioIngles, Utils.createGridBagConstraints(1, 1));
+        this.add(radioPortugues, Utils.createGridBagConstraints(2, 1));
         JButton btnTraduzir = new JButton("Traduzir");
         btnTraduzir.setIcon(getIconForButton("Edit24.gif"));
         this.add(btnTraduzir, Utils.createGridBagConstraints(3, 1));
